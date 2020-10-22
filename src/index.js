e0bda9abde505feaa996079821a706a8787b8897
ilya-kupreichyk <kupreichykilya@gmail.com>
2020-10-22 22:43:57 +0300

    Update index.js
    feat: create functions: reverse int to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+   const str = String(Math.abs(n));
+    let result = '';
+
+    for (let i = str.length; i > 0; i--) {
+        result += str[i - 1];
+    }
+
+    return Number(result);
 }
